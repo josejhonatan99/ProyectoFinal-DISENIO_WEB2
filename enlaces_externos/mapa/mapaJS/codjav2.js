bc6830bab9d74b5400d2c0d8d71781da766a73c8
josejhonatan99 <cb.jose.silvestre.a@upds.net.bo>
2021-05-03 18:01:23 -0400

    actt1

---
+++ enlaces_externos/mapa/mapaJS/codjav2.js
@@ -0,0 +1,235 @@
+
+function efecto(){
+    $('.caja').show();
+    $('.caja2').hide();
+    $('.caja3').hide();
+    $('.caja4').hide();
+    $('.caja5').hide();
+    $('.caja6').hide();
+    $('.caja7').hide();
+    $('.caja8').hide();
+    $('.caja9').hide();
+    $('.caja10').hide();
+
+    //para el cuadro de imagenes
+    $('.box').show();
+    $('.box2').hide();
+    $('.box3').hide();
+    $('.box4').hide();
+    $('.box5').hide();
+    $('.box6').hide();
+    $('.box7').hide();
+    $('.box8').hide();
+    $('.box9').hide();
+    $('.box10').hide();
+   
+}
+
+function efecto2(){
+    $('.caja').hide();
+    $('.caja2').show();
+    $('.caja3').hide();
+    $('.caja4').hide();
+    $('.caja5').hide();
+    $('.caja6').hide();
+    $('.caja7').hide();
+    $('.caja8').hide();
+    $('.caja9').hide();
+    $('.caja10').hide();
+
+    $('.box').hide();
+    $('.box2').show();
+    $('.box3').hide();
+    $('.box4').hide();
+    $('.box5').hide();
+    $('.box6').hide();
+    $('.box7').hide();
+    $('.box8').hide();
+    $('.box9').hide();
+    $('.box10').hide();
+}
+function efecto3(){
+    $('.caja').hide();
+    $('.caja2').hide();
+    $('.caja3').show();
+    $('.caja4').hide();
+    $('.caja5').hide();
+    $('.caja6').hide();
+    $('.caja7').hide();
+    $('.caja8').hide();
+    $('.caja9').hide();
+    $('.caja10').hide();
+
+    $('.box').hide();
+    $('.box2').hide();
+    $('.box3').show();
+    $('.box4').hide();
+    $('.box5').hide();
+    $('.box6').hide();
+    $('.box7').hide();
+    $('.box8').hide();
+    $('.box9').hide();
+    $('.box10').hide();
+}
+function efecto4(){
+    $('.caja').hide();
+    $('.caja2').hide();
+    $('.caja3').hide();
+    $('.caja4').show();
+    $('.caja5').hide();
+    $('.caja6').hide();
+    $('.caja7').hide();
+    $('.caja8').hide();
+    $('.caja9').hide();
+    $('.caja10').hide();
+
+    $('.box').hide();
+    $('.box2').hide();
+    $('.box3').hide();
+    $('.box4').show();
+    $('.box5').hide();
+    $('.box6').hide();
+    $('.box7').hide();
+    $('.box8').hide();
+    $('.box9').hide();
+    $('.box10').hide();
+}
+function efecto5(){
+    $('.caja').hide();
+    $('.caja2').hide();
+    $('.caja3').hide();
+    $('.caja4').hide();
+    $('.caja5').show();
+    $('.caja6').hide();
+    $('.caja7').hide();
+    $('.caja8').hide();
+    $('.caja9').hide();
+    $('.caja10').hide();
+
+    $('.box').hide();
+    $('.box2').hide();
+    $('.box3').hide();
+    $('.box4').hide();
+    $('.box5').show();
+    $('.box6').hide();
+    $('.box7').hide();
+    $('.box8').hide();
+    $('.box9').hide();
+    $('.box10').hide();
+}
+function efecto6(){
+    $('.caja').hide();
+    $('.caja2').hide();
+    $('.caja3').hide();
+    $('.caja4').hide();
+    $('.caja5').hide();
+    $('.caja6').show();
+    $('.caja7').hide();
+    $('.caja8').hide();
+    $('.caja9').hide();
+    $('.caja10').hide();
+
+    $('.box').hide();
+    $('.box2').hide();
+    $('.box3').hide();
+    $('.box4').hide();
+    $('.box5').hide();
+    $('.box6').show();
+    $('.box7').hide();
+    $('.box8').hide();
+    $('.box9').hide();
+    $('.box10').hide();
+}
+function efecto7(){
+    $('.caja').hide();
+    $('.caja2').hide();
+    $('.caja3').hide();
+    $('.caja4').hide();
+    $('.caja5').hide();
+    $('.caja6').hide();
+    $('.caja7').show();
+    $('.caja8').hide();
+    $('.caja9').hide();
+    $('.caja10').hide();
+
+    $('.box').hide();
+    $('.box2').hide();
+    $('.box3').hide();
+    $('.box4').hide();
+    $('.box5').hide();
+    $('.box6').hide();
+    $('.box7').show();
+    $('.box8').hide();
+    $('.box9').hide();
+    $('.box10').hide();
+}
+function efecto8(){
+    $('.caja').hide();
+    $('.caja2').hide();
+    $('.caja3').hide();
+    $('.caja4').hide();
+    $('.caja5').hide();
+    $('.caja6').hide();
+    $('.caja7').hide();
+    $('.caja8').show();
+    $('.caja9').hide();
+    $('.caja10').hide();
+
+    $('.box').hide();
+    $('.box2').hide();
+    $('.box3').hide();
+    $('.box4').hide();
+    $('.box5').hide();
+    $('.box6').hide();
+    $('.box7').hide();
+    $('.box8').show();
+    $('.box9').hide();
+    $('.box10').hide();
+}
+function efecto9(){
+    $('.caja').hide();
+    $('.caja2').hide();
+    $('.caja3').hide();
+    $('.caja4').hide();
+    $('.caja5').hide();
+    $('.caja6').hide();
+    $('.caja7').hide();
+    $('.caja8').hide();
+    $('.caja9').show();
+    $('.caja10').hide();
+
+    $('.box').hide();
+    $('.box2').hide();
+    $('.box3').hide();
+    $('.box4').hide();
+    $('.box5').hide();
+    $('.box6').hide();
+    $('.box7').hide();
+    $('.box8').hide();
+    $('.box9').show();
+    $('.box10').hide();
+}
+function efecto10(){
+    $('.caja').hide();
+    $('.caja2').hide();
+    $('.caja3').hide();
+    $('.caja4').hide();
+    $('.caja5').hide();
+    $('.caja6').hide();
+    $('.caja7').hide();
+    $('.caja8').hide();
+    $('.caja9').hide();
+    $('.caja10').show();
+
+    $('.box').hide();
+    $('.box2').hide();
+    $('.box3').hide();
+    $('.box4').hide();
+    $('.box5').hide();
+    $('.box6').hide();
+    $('.box7').hide();
+    $('.box8').hide();
+    $('.box9').hide();
+    $('.box10').show();
+}
+
